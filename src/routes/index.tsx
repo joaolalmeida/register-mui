bf4f731c65f509d6d2cb419718f906d1956047f0
joaolalmeida <joaolalmeida20@gmail.com>
2023-07-06 16:53:18 -0300

    feat: Adicionado tema claro e provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@mui/material'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/pagina-inicial' element={<p>Página inicial</p>} />
+      <Route path='/pagina-inicial' element={<Button variant='contained' color='primary'>teste</Button>} />
 
       <Route path='*' element={<Navigate to="/pagina-inicial"/>} />
       {/* Navigate: redireciona para a pagina escolhida  */}
