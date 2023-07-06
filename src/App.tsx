e7a83fbf69062f0b352e4afe87e572ecad895f41
joaolalmeida <joaolalmeida20@gmail.com>
2023-07-06 15:07:11 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes"
+
 export function App() {
 
   return (
-    <div>
-      <h1>teste</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
